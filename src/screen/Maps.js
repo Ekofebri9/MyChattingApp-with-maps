1368a5822d23bed167de7bc676a3341f761bf422
eko <febri1412@gmail.com>
2019-07-16 23:29:25 +0700

    versi 2

--- src/screen/Maps.js
+++ src/screen/Maps.js
@@ -1,25 +1,43 @@
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-export default class Map extends Component { 
-    render() {
-        return (
-        <MapView provider={ PROVIDER_GOOGLE }
-                    style={{ flex: 1, width: window.width }} //window pake Dimensions
-                    region={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421 
-                    }} >
-                    <MapView.Marker
-                    coordinate={{
-                        latitude: 38.78825,
-                        longitude: -122.4324,
-                    }}
-                    title="Lokasi"
-                    description="Hello" />
-                </MapView>
-        )  
+import {View, Text, TouchableOpacity, TextInput, AsyncStorage, Dimensions} from 'react-native';
+const {width,height} = Dimensions.get('window')
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = -7.7584436;
+const LONGITUDE = 110.3759749;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+class Maps extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            region: {
+                latitude: LATITUDE,
+                longitude: LONGITUDE,
+                latitudeDelta: LATITUDE_DELTA,
+                longitudeDelta: LONGITUDE_DELTA,
+            },
+        }
+    }
+
+    static navigationOptions = {
+        header:null
+    }
+      
+    onRegionChange = (region) => {
+        this.setState({ region });
+    }
+    render(){
+        return(
+            <MapView
+                style={{flex:1, width:width}}
+                provider={PROVIDER_GOOGLE}
+                region={this.state.region}
+            />
+        )
     }
 }
+
+export default Maps
